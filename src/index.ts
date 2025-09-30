890ebb5cd1acc20f036ebaf0dcfffebfbde1d700
Bruno Santos <bruno.if@hotmail.com>
2025-09-30 20:15:05 -0300

    Barrel export file organized

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,40 @@
 // Barrel export file
 
-// buildingElements
-export { default as Aggregate } from './structuralElements/Aggregate.js';
+// structuralElements
 export { default as Concrete } from './structuralElements/Concrete.js';
 export { default as PrestressingSteel } from './structuralElements/PrestressingSteel.js';
 export { default as Steel } from './structuralElements/Steel.js';
 export type { IPrestressingSteel, PrestressingSteelLabel, RelaxationType } from './structuralElements/PrestressingSteel.js';
 
 // combinations
-export { Combinations, Qsi1, Qsi2 } from './combinationLoads/Load.js';
+export { Combinations, QuasiPermanent, Frequent, Rare, Last, Qsi1, Qsi2 } from './combinationLoads/Load.js';
 
 // structuralDesign
 export { CableGeometry } from './structuralDesign/prestressingSteel/CableGeometry.js';
-export { default as PrestressingSteelEstimated } from './structuralDesign/prestressingSteel/PrestressingSteelEstimated.js';
+export { default as PrestressingDesign } from './structuralDesign/prestressingSteel/PrestressingSteelEstimated.js';
+export { AbstractPrestressingSteelDesign, LimitedPrestressingSteelDesign, CompletePrestressingSteelDesign } from './structuralDesign/prestressingSteel/PrestressingSteelEstimated.js';
+export { default as PrestressingSteelForce } from './structuralDesign/prestressingSteel/PrestressingSteelForce.js';
+export { default as CompressionStruts } from './structuralDesign/prestressingSteel/CompressionStruts.js';
+export { default as ShearSteel } from './structuralDesign/prestressingSteel/ShearSteel.js';
+export { default as ReinforcingSteelLower } from './structuralDesign/prestressingSteel/ReinforcingSteelLower.js';
+export { default as ReinforcingSteelUpper } from './structuralDesign/prestressingSteel/ReinforcingSteelUpper.js';
 
-// structuralDesign/Losses
+// structuralDesign/losses
 export { default as AnchorageLoss } from './structuralDesign/prestressingSteel/losses/AnchorageLoss.js';
 export { default as ElasticShorteningLoss } from './structuralDesign/prestressingSteel/losses/ElasticShorteningLoss.js';
 export { default as FrictionLoss, type AnchoringType } from './structuralDesign/prestressingSteel/losses/FrictionLoss.js';
 export { default as TimeDependentLoss } from './structuralDesign/prestressingSteel/losses/TimeDependentLoss.js';
 
+// structuralDesign/limitStates
+export { default as ELS } from './structuralDesign/prestressingSteel/limitStates/ELS.js';
+export { default as ELU } from './structuralDesign/prestressingSteel/limitStates/ELU.js';
+export { default as ELSDEF } from './structuralDesign/prestressingSteel/limitStates/ELSDEF.js';
+
+// structuralDesign/concrete
+export { CreepConcrete } from './structuralDesign/concrete/Creep.js';
+
 // types
-export type { ValueUnit, ValuesUnit } from './types/index.js';
+export type { ValueUnit, ValuesUnit, Distance, Angles, Verification, VerificationOneValue, VerificationResult } from './types/index.js';
+export type { IGeometricProperties } from './types/combinationsType.js';
+export type { PrestressingDesignType } from './types/prestressSteelType.js';
+export type { GeometricPropsWithUnitsType } from './types/sectionsType.js';
